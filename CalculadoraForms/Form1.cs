217fd1463b9d98ed4dc3c41c569dadab70b2910c
Lucas Araujo <149210048+Lucas2k23@users.noreply.github.com>
2023-11-06 21:45:49 -0300

    interface e codigos completos
    calculadora basica feita no windows forms, com os devidos tratamentos de erros.

--- CalculadoraForms/Form1.cs
+++ CalculadoraForms/Form1.cs
@@ -12,8 +12,11 @@ namespace CalculadoraForms
 {
     public partial class Form1 : Form
     {
+
         int numero1;
         string ultimoOp;
+        bool Contador = false;
+        
         public Form1()
         {
             InitializeComponent();
@@ -21,16 +24,39 @@ namespace CalculadoraForms
 
         private void btnLimpar_Click(object sender, EventArgs e)
         {
+            Contador = false;
             txbTela.Clear();
+            txbAux.Clear();
         }
         private void Operador_Click(object sender, EventArgs e)
         {
-            // Obter o botão que está chamando o evento
             var botao = (Button)sender;
-            numero1 = int.Parse(txbTela.Text);
-            txbTela.Clear();
-            txbAux.Text = numero1.ToString() + botao.Text;
-            ultimoOp = botao.Text;
+            
+            if (Contador == false && txbTela.Text != "" && txbAux.Text == "")
+            {
+                // Obter o botão que está chamando o evento
+                
+                numero1 = int.Parse(txbTela.Text);
+                txbTela.Clear();
+                txbAux.Text = numero1.ToString() + botao.Text;
+                ultimoOp = botao.Text;
+                Contador = true;
+            }
+            else
+            {
+                if(txbTela.Text != "")
+                {
+                    btnIgual.PerformClick();
+                    numero1 = int.Parse(txbTela.Text);
+                    txbAux.Text = txbTela.Text + botao.Text;
+                    numero1 = int.Parse(txbTela.Text);
+                    txbTela.Text = "";
+                    ultimoOp = botao.Text;
+                    
+
+                }
+                
+            }
 
         }
         private void Numero_Click(object sender, EventArgs e)
@@ -38,17 +64,69 @@ namespace CalculadoraForms
             // Obter o botão que está chamando o evento
             var botao = (Button)sender;
             txbTela.Text += botao.Text;
+           
         }
 
         private void btnIgual_Click(object sender, EventArgs e)
         {
-            switch (ultimoOp)
+            
+
+           if(txbTela.Text != "")
             {
-                case "+":
-                    txbAux.Clear();
-                    txbTela.Text = (numero1 + int.Parse(txbTela.Text)).ToString();
-                    break;
+                switch (ultimoOp)
+                {
+
+                    case "+":
+                        
+                        txbAux.Clear();
+                        txbTela.Text = (numero1 + int.Parse(txbTela.Text)).ToString();
+                        break;
+                    case "-":
+                        
+                        txbAux.Clear();
+                        txbTela.Text = (numero1 - int.Parse(txbTela.Text)).ToString();
+                        break;
+                    case "x":
+                        
+                        txbAux.Clear();
+                        txbTela.Text = (numero1 * int.Parse(txbTela.Text)).ToString();
+
+                        break;
+                    case "÷":
+                        
+                        if (int.Parse(txbTela.Text) != 0)
+                        {
+                            txbAux.Clear();
+                            txbTela.Text = (numero1 / int.Parse(txbTela.Text)).ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Impossivel dividir por zero!");
+                            txbTela.Clear();
+                            txbAux.Clear();
+                        }
+
+
+                        break;
+
+                }
             }
+            else
+            {
+                MessageBox.Show("Digite algum valor antes de clicar no igual");
+            }
+
+            Contador = false;
+        }
+
+        private void txbTela_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbAux_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
